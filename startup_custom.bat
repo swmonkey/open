bc4b0713bd361f39dd1f0619f688c33639f4e4e3
swmonkey <davidhc360@gmail.com>
2020-06-18 20:28:45 +0100

    Add files via upload

---
+++ startup_custom.bat
@@ -0,0 +1,14 @@
+//v0.1
+//copy to C:\Users\davidhc\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+//work allocation
+//work servers,apps
+
+//home none
+//kill teams
+//
+
+timeout /t 5
+
+"PATH TO IMAGE HERE"
+
+"PATH TO File HERE"
